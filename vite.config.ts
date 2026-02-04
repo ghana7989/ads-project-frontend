862cc8d59198105c9f3d99ee9db3b9f62b93a709
Pavan Chindukuri <chindukuripavan@gmail.com>
2026-02-04 11:00:17 +0530

    Update dependencies and configure Netlify plugins in Vite setup
    - Added '@netlify/vite-plugin-react-router' and '@netlify/vite-plugin' to package.json and package-lock.json for enhanced routing and deployment capabilities.
    - Updated Vite configuration to include the new Netlify plugins for improved integration.

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import netlifyReactRouter from '@netlify/vite-plugin-react-router' // <- add this
+import netlify from '@netlify/vite-plugin' // <- add this (optional)
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
+	plugins: [
+		react({
+			babel: {
+				plugins: [['babel-plugin-react-compiler']],
+			},
+		}),
+		netlifyReactRouter(), // <- add this
+		netlify(), // <- add this (optional)
+	],
 })
